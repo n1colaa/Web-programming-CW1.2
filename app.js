2c7a408b36022875598004d586e635dba85d4631
j <shahidjunaid0131@protonmail.com>
2025-11-14 21:53:16 +0000

    did some edits

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
 app.get("/", (req, res) => {
   res.sendFile("public/index.html", { root: __dirname });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get('/new-table', (req, res) => {
+    res.render('new-table'); 
 });
 
 app.listen(port, () => {
   console.log(`Sandbox listening on port ${port}`);
 });
 
-app.use(express.static("public"));
 
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
