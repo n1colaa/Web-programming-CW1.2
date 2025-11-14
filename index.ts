e0feffddba0dda74cf98d9a43ed0ace6462053e9
n1colaa <nicolaayedogbon12@gmail.com>
2025-11-14 17:36:13 +0000

    converted index.html to ejs

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile("public/index.html", {root: __dirname});
+  res.sendFile("public/index.html", { root: __dirname });
 });
 
 app.listen(port, () => {
@@ -12,3 +12,8 @@ app.listen(port, () => {
 
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
