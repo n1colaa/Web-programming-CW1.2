89a4663162bc30054ab6bda8dbcb2fa3147535ff
j <shahidjunaid0131@protonmail.com>
2025-11-14 21:16:00 +0000

    edited app.js and edited .ejs files

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.listen(port, () => {
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
   res.render("index");
