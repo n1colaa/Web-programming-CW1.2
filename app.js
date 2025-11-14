5760ff4f8fc021909d19b20c87299a6e7b815254
j <shahidjunaid0131@protonmail.com>
2025-11-14 21:22:46 +0000

    edited app.js

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.listen(port, () => {
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
   res.render("index");
