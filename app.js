8c439e7db9497ecd123e8223a9f36fe2389b6b2a
j <shahidjunaid0131@protonmail.com>
2025-11-15 16:10:46 +0000

    added sql db

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mysql = require("mysql");
 
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: "sql8.freesqldatabase.com",
+    user: "sql8807974",
+    password: "HkHxHIDR42",
+    database: "sql8807974",
+});
+
+pool.getConnection(function(err, con) {
+    if (err) throw err;
+    console.log("Connected!");
+    //...
+});
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
