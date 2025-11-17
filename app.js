39f1c17b60dca6656206bb36c8ce4e83a5d0c7eb
j <shahidjunaid0131@protonmail.com>
2025-11-17 19:34:55 +0000

    deleted the db folder, fixed some stuff, problem is req.values dont work

--- app.js
+++ app.js
@@ -11,10 +11,52 @@ const pool = mysql.createPool({
     database: "sql8807974",
 });
 
-pool.getConnection(function(err, con) {
+function queryDatabase(query, callback) {
+    pool.getConnection((err, connection) => {
+        if (err) {
+            return callback(err, null);
+        } else if (connection) {
+            connection.query(query, (err, rows, fields) => {
+                connection.release();
+                if (err) {
+                    return callback(true, err);
+                }
+                return callback(null, rows);
+            });
+        } else {
+            return callback(true, "No connection");
+        }
+    });
+}
+
+pool.getConnection(function(err, con){
     if (err) throw err;
-    console.log("Connected!");
-    //...
+    console.log("Connected");
+    con.query("CREATE TABLE IF NOT EXISTS reservation (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), partySize TINYINT, bookingDate VARCHAR(10), bookingTime VARCHAR(5), phoneNum VARCHAR(10), email VARCHAR(255) UNIQUE NOT NULL)", (err)=>{
+        con.release();
+        if (err) throw err;
+        console.log("Created table");
+    })
+})
+
+app.post("/", function (req, res,) {
+    console.log(req.fname);
+    console.log(req.size);
+    console.log(req.date);
+    console.log(req.time);
+    console.log(req.contactNum);
+    console.log(req.email);
+    pool.getConnection(function (err, con) {
+        if (err) throw err;
+        console.log("Connected");
+//        const sql = "INSERT INTO reservation (name, partySize, bookingDate , bookingTime, phoneNum, Email) VALUES ({req.fname}, '${req.size}', '${req.date}', '${req.time}', '${req.contactNum}', '${req.email}')";
+//
+  //      con.query(sql, function (err, result){
+    //        con.release();
+      //      if (err) throw err;
+        //    console.log("1 record inserted!");
+       // })
+    });
 });
 
 app.use(express.static("public"));
